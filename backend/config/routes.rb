c6a115992c6f485d404890fe3be9010f721374fd
Rimane <rimane.o@gmail.com>
2025-05-24 17:20:11 +0900

    rubocop -a で一部修正 #1

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "/name", to: ->(env) { [200, { "Content-Type" => "application/json" }, [{name: "太郎"}.to_json()]] }
+  get '/name', to: ->(_env) { [200, { 'Content-Type' => 'application/json' }, [{ name: '太郎' }.to_json]] }
 
   # 単純に JSON {"status":"OK"} を返すヘルスチェック
-  get "/health", to: ->(env) { [200, { "Content-Type" => "application/json" }, [ { status: "OK" }.to_json ]] }
+  get '/health', to: ->(_env) { [200, { 'Content-Type' => 'application/json' }, [{ status: 'OK' }.to_json]] }
 end

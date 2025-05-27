f985afb5326215809d1793d1b125ec0644c7035a
Rimane <rimane.o@gmail.com>
2025-05-27 18:24:19 +0900

    適当なユーザーと料理を seed できるようにした #4

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# ユーザー
+[
+  { name: 'デモシェフ',  role: 'admin' },
+  { name: 'デモユーザー', role: 'user' }
+].each do |attrs|
+  # `find_or_create_by!`を使用して、ユーザーが存在しない場合は作成
+  User.find_or_create_by!(name: attrs[:name]) do |u|
+    u.role = attrs[:role]
+  end
+end
+
+# 料理
+[
+  { name: 'チャーハン', price: 700 },
+  { name: '餃子', price: 450 },
+  { name: '麻婆豆腐', price: 800 }
+].each do |attrs|
+  # `find_or_create_by!`を使用して、料理が存在しない場合は作成
+  Dish.find_or_create_by!(name: attrs[:name]) do |d|
+    d.price = attrs[:price]
+  end
+end

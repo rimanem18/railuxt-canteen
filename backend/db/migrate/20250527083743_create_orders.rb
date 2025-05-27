44892b2d915a8a93fb9a22abac4fc0cf7b46dcd2
Rimane <rimane.o@gmail.com>
2025-05-27 17:53:36 +0900

    注文テーブルのマイグレーションで、quantityのデフォルト値を1に設定し、statusのnull制約を追加 #4

--- backend/db/migrate/20250527083743_create_orders.rb
+++ backend/db/migrate/20250527083743_create_orders.rb
@@ -5,8 +5,8 @@ class CreateOrders < ActiveRecord::Migration[7.0]
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.references :dish, null: false, foreign_key: true
-      t.integer :quantity
-      t.boolean :status, null: false, default: false
+      t.integer :quantity, default: 1, null: false
+      t.boolean :status, default: false, null: false
 
       t.timestamps
     end

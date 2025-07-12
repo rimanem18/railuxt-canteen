a793fbaba7ab35302852acefea84fa65a3d96b3f
Rimane <rimane.o@gmail.com>
2025-07-12 09:04:32 +0900

    Devise と DeviseTokenAuth を設定し、ユーザー認証機能を追加 #10

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
   has_many :orders, dependent: :destroy
 end
